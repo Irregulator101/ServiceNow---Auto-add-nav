0b22a6463f45732802704ee884f414775b93f12b
Eric Schultz <ericschultz101@gmail.com>
2024-12-31 10:34:34 -0800

    Remove unnecessary noncapture group regex

--- userscript.js
+++ userscript.js
@@ -1,21 +1,21 @@
 // ==UserScript==
 // @name           ServiceNow - Auto-add nav frame
 // @namespace      Violentmonkey Scripts
-// @include        /https?:\/\/.+?\.(?:service-now|servicenowservices)\.com\/.+?\.do(.+?|$)/
-// @include        /https?:\/\/.+?\.(?:service-now|servicenowservices)\.com\/sp_config(.+?|$)/
-// @exclude        /https?:\/\/.+?\.(?:service-now|servicenowservices)\.com\/now\/nav\/ui\/classic\/params\/target/.+?/
-// @exclude        /https?:\/\/.+?\.(?:service-now|servicenowservices)\.com\/login\.do/
-// @exclude        /https?:\/\/.+?\.(?:service-now|servicenowservices)\.com\/login_redirect\.do/
-// @exclude        /https?:\/\/.+?\.(?:service-now|servicenowservices)\.com\/validate_multifactor_auth_code\.do/
-// @exclude        /https?:\/\/.+?\.(?:service-now|servicenowservices)\.com\/side_door\.do/
-// @exclude        /https?:\/\/.+?\.(?:service-now|servicenowservices)\.com\/navpage\.do/
-// @exclude        /https?:\/\/.+?\.(?:service-now|servicenowservices)\.com\/logout_success\.do/
-// @exclude        /https?:\/\/.+?\.(?:service-now|servicenowservices)\.com\/\$.+/
-// @exclude        /https?:\/\/.+?\.(?:service-now|servicenowservices)\.com\/sys_store_app\.do/
-// @exclude        /https?:\/\/.+?\.(?:service-now|servicenowservices)\.com\/session_timeout\.do/
-// @exclude        /https?:\/\/.+?\.(?:service-now|servicenowservices)\.com\/auth_redirect\.do/
-// @exclude        /https?:\/\/.+?\.(?:service-now|servicenowservices)\.com\/x_snc_sso_auth\.do/
-// @exclude        /https?:\/\/.+?\.(?:service-now|servicenowservices)\.com\/workflow_ide\.do(?:.+?|$)/
+// @include        /https?:\/\/.+?\.(service-now|servicenowservices)\.com\/.+?\.do(.+?|$)/
+// @include        /https?:\/\/.+?\.(service-now|servicenowservices)\.com\/sp_config(.+?|$)/
+// @exclude        /https?:\/\/.+?\.(service-now|servicenowservices)\.com\/now\/nav\/ui\/classic\/params\/target/.+?/
+// @exclude        /https?:\/\/.+?\.(service-now|servicenowservices)\.com\/login\.do/
+// @exclude        /https?:\/\/.+?\.(service-now|servicenowservices)\.com\/login_redirect\.do/
+// @exclude        /https?:\/\/.+?\.(service-now|servicenowservices)\.com\/validate_multifactor_auth_code\.do/
+// @exclude        /https?:\/\/.+?\.(service-now|servicenowservices)\.com\/side_door\.do/
+// @exclude        /https?:\/\/.+?\.(service-now|servicenowservices)\.com\/navpage\.do/
+// @exclude        /https?:\/\/.+?\.(service-now|servicenowservices)\.com\/logout_success\.do/
+// @exclude        /https?:\/\/.+?\.(service-now|servicenowservices)\.com\/\$.+/
+// @exclude        /https?:\/\/.+?\.(service-now|servicenowservices)\.com\/sys_store_app\.do/
+// @exclude        /https?:\/\/.+?\.(service-now|servicenowservices)\.com\/session_timeout\.do/
+// @exclude        /https?:\/\/.+?\.(service-now|servicenowservices)\.com\/auth_redirect\.do/
+// @exclude        /https?:\/\/.+?\.(service-now|servicenowservices)\.com\/x_snc_sso_auth\.do/
+// @exclude        /https?:\/\/.+?\.(service-now|servicenowservices)\.com\/workflow_ide\.do(.+?|$)/
 // @grant          none
 // @version        0.5
 // @author         Eric Schultz
